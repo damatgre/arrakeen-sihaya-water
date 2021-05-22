da1a30a74dcc06fe9de87db517492516feab2494
damatgre <gregdamato2@gmail.com>
2021-05-22 15:28:47 -0400

    added list functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,29 @@
+var apiKey = "&27e890ac1ac349b8058e8451ef106db3"
+
 //setting city 
 var city = $("#searchCity").val();
+var temp = $("#tempEl");
+var wind = $("#windEl");
+var humidity = $("humidEl");
+var uvIn = $("uvEl");
+
+$("#searchBtn").on("click", function () {
+
+    //get the value from the input
+    city = $("#searchCity").val();
+    //clear value from the input
+    $("#searchCity").val("");
+
+    console.log(city);
+    //additional function area
+    addList();
+});
+
+//function to add list items search a city card
+function addList() {
+    if (city){
+        var listItem = $("<li>").addClass("list-group-item").text(city);
+        $(".list").append(listItem);
+    }
+};
 
